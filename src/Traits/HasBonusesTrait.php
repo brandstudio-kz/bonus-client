9207af0c7c889fc53bfc2f1cf1cc07c0f9032120
Nurdaulet <nurik9293709@gmail.com>
2020-02-19 18:35:21 +0600

    hasBonuses Trait

---
+++ src/Traits/HasBonusesTrait.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace BrandStudio\Bonus\Traits;
+
+use BrandStudio\Bonus\Facades\Bonus;
+
+trait HasBonusesTrait
+{
+    protected $cache_client;
+
+    public function getClientAttribute()
+    {
+        if ($this->cache_client) {
+            return $this->cache_client;
+        }
+
+        return $this->cache_client = Bonus::getClient([
+            'client' => [
+                'phone' => $this->phone,
+                'full_name' => $this->full_name,
+            ]
+        ])['data'] ?? null;
+    }
+
+    public function getBonusesAttribute()
+    {
+        return $this->client ? ($this->client['bonuses'] ?? []) : [];
+    }
+
+    public function getBonusAttribute()
+    {
+        return $this->client ? $this->client['bonus'] : 0;
+    }
+
+}
