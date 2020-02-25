0e5604c50852a662b6c3d0d19cdc56bc918cdc0e
Nurdaulet <nurik9293709@gmail.com>
2020-02-26 05:00:04 +0600

    clonfig client id

--- src/config/bonus.php
+++ src/config/bonus.php
@@ -5,5 +5,6 @@ return [
     // STATUS options: SUCCESS => 1, PENDING => 2, NOT_VERIFIED => 3, CANCELED => 4
     // TYPE options: SHOP => 1, SITE => 2, EXTRA => 3
     'bonus_type' => env('BONUS_TYPE', 2),
+    'bonus_site_id' => env('BONUS_SITE_ID', 1),
     'token' => env('BONUS_TOKEN', 'testtoken'),
 ];
